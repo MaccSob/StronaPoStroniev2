83b23f4e27398cea1c6e39195a8d7a0af94d12b7
MaccSob <71sobmaciej@gmail.com>
2025-12-07 23:57:10 +0100

    nodemailer

--- app/api/email/email.ts
+++ app/api/email/email.ts
@@ -1,5 +1,19 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
 
 export async function POST(request: NextRequest) {
   return NextResponse.json('Hello from API!');
 }
+
+
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: "me@gmail.com",
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+  },
+});
