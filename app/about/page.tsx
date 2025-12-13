f020911be180991c480626992c1f7fd65125cd9f
MaccSob <71sobmaciej@gmail.com>
2025-12-13 23:58:00 +0100

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,12 +4,21 @@ export default function About() {
   return (
   
  <div className="min-h-screen flex items-center justify-center">
-    <div className="space-y3 max-w-3xl">
-      <h1 className="text-5xl font-semibold text-center">O mnie</h1>
-      <p className="text-black-200">
-Cześć, mam na imię Izabela i zajmuję się korektą, redakcją i składem tekstów. Uwielbiam pracować ze słowami i dbać o to, aby każdy tekst był poprawny, czytelny i atrakcyjny.       </p>
-    </div>
-    
+
+<section className="py-24 relative">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg:px-5 mx-auto">
+            <div className="w-full justify-start items-center gap-8 grid lg:grid-cols-2 grid-cols-1">
+                <div className="w-full flex-col justify-start lg:items-start items-center gap-10 inline-flex">
+                    <div className="w-full flex-col justify-start lg:items-start items-center gap-4 flex">
+                        <h2 className="text-gray-900 text-4xl font-bold font-manrope leading-normal lg:text-start text-center">Cześć, mam na imię Izabela i zajmuję się korektą, redakcją i składem tekstów. Uwielbiam pracować ze słowami i dbać o to, aby każdy tekst był poprawny, czytelny i atrakcyjny. </h2>
+                        <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center"> W wolnych chwilach zajmuję się moją kolekcją roślin doniczkowych, która liczy już ponad 50 egzemplarzy. To moja pasja i sposób na relaks. Lubię też czytać książki i grać w gry planszowe, co pozwala mi przeżywać przygody, poznawać nowe historie, a także rozwijać umiejętność logicznego myślenia i strategii. A gdy mam ochotę na coś słodkiego, piekę ciasta i dzielę się nimi z rodziną – znikają w mgnieniu oka.</p>
+                   <p className="text-gray-500 text-base font-normal leading-relaxed lg:text-start text-center">Nieważne, czy piszesz książki, e-booki, prace dyplomowe (doktorskie, magisterskie, licencjackie), tworzysz teksty na strony internetowe, blogi, do social mediów – zawsze warto dbać o poprawność językową, a ja chętnie Ci w tym pomogę. Skontaktuj się ze mną, aby omówić szczegóły i poznać indywidualną ofertę. </p>
+                    </div>
+                </div>
+                <img className="lg:mx-0 mx-auto h-full rounded-3xl object-cover" src="https://stronapostronie.pl/assets/image/Izabela.jpg" alt="pic" />
+            </div>
+        </div>
+    </section>
   </div>
   );
 }
