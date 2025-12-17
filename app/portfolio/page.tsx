b035448f9b17bd09a6b547e17097d21e70928ad0
MaccSob <71sobmaciej@gmail.com>
2025-12-17 23:56:51 +0100

    footer

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,4 +1,4 @@
-
+import '../styles/portfolio.scss'
 
 export default function Portfolio() {
   return (
@@ -6,7 +6,15 @@ export default function Portfolio() {
     <div className="min-h-screen flex items-center justify-center">
     <div className="space-y3 max-w-5xl">
         <h1 className="text-5xl font-semibold text-center">Portfolio:</h1>
+
+        <div className="imgcontainer">
         <img src="./szymanski.png" alt="" />
+           <img src="./dzwiekoterapia.jpg" alt="" />
+              <img src="./rose.jpg" alt="" />
+                 <img src="./szymanski.jpg" alt="" />
+                    <img src="./zacznij.jpg" alt="" />
+                       <img src="./galopem.jpg" alt="" />
+                       </div>
 
 
     </div>
