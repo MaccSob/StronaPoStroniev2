8957e8e45715e9cd88e1c8a406eab076d498b649
MaccSob <71sobmaciej@gmail.com>
2025-12-20 23:56:58 +0100

    policy

---
+++ app/policy/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Policy() {
+  return (
+  
+<>
+<h1>Informacje Ogólne</h1>
+<h1>Hosting</h1>
+<h1>Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych</h1>
+<h1>Informacje w formularzach</h1>
+<h1>Informacja o plikach cookies</h1>
+
+</>
+  );
+}
+
+
+
+
+
