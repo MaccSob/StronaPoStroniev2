ce6f4ac8fd31a25dc896166c08585ff74b380d21
MaccSob <71sobmaciej@gmail.com>
2025-12-25 23:58:17 +0100

    policy still

--- app/policy/page.tsx
+++ app/policy/page.tsx
@@ -16,12 +16,12 @@ export default function Policy() {
             <li> Kontakt z Administratorem jest możliwy za pośrednictwem poczty e-mail: izabela@stronapostronie.pl </li>
             <li> Operator jest Administratorem Twoich danych osobowych w odniesieniu do danych podanych dobrowolnie w Serwisie. </li>
             <li> Serwis wykorzystuje dane osobowe w następujących celach: 
-                 Obsługa zapytań przez formularz.
+              <ol>  Obsługa zapytań przez formularz. </ol> 
 
             </li>
             <li> Serwis realizuje funkcje pozyskiwania informacji o użytkownikach i ich zachowaniu w następujący sposób: 
-                 Poprzez dobrowolnie wprowadzone w formularzach dane, które zostają wprowadzone do systemów Operatora. 
-                  Poprzez zapisywanie w urządzeniach końcowych plików cookie (tzw. „ciasteczka”). 
+               <ol>  Poprzez dobrowolnie wprowadzone w formularzach dane, które zostają wprowadzone do systemów Operatora.  </ol> 
+                <ol>   Poprzez zapisywanie w urządzeniach końcowych plików cookie (tzw. „ciasteczka”).  </ol>
             </li>
         </ul>
 
@@ -34,81 +34,97 @@ export default function Policy() {
         </ul>
 
         <h2 className="text-2xl font-bold mb-2"> Hosting</h2>
+
+                   <ul className="list-disc list-inside mb-4">
+
 <li> Serwis jest hostowany (technicznie utrzymywany) na serwerach operatora: seohost.pl b</li>
+<li>  Firma hostingowa w celu zapewnienia niezawodności technicznej prowadzi logi na poziomie serwera. Zapisowi mogą podlegać: </li>
+<li>   Zasoby określone identyfikatorem URL (adresy żądanych zasobów – stron, plików). </li>
+<li> Czas nadejścia zapytania</li>
+<li> Czas wysłania odpowiedzi</li>
+<li> Adres URL strony poprzednio odwiedzanej przez użytkownika (referer link) – w przypadku gdy przejście do Serwisu nastąpiło przez odnośnik. </li>
+<li> Informacje o przeglądarce użytkownika</li>
+<li> Informacje o adresie IP. </li>
+<li> Informacje diagnostyczne związane z procesem samodzielnego zamawiania usług poprzez rejestratory na stronie. </li>
+<li>   Informacje związane z obsługą poczty elektronicznej kierowanej do Operatora oraz wysyłanej przez Operatora. </li>
+ </ul>
+       
+     <h2 className="text-2xl font-bold mb-2"> Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych </h2>
+       
+           <ul className="list-disc list-inside mb-4">
 
-        <h2 className="text-2xl font-bold mb-2"> Twoje prawa i dodatkowe informacje o sposobie wykorzystania danych </h2>
-
-        <p className="mb-4">
-            A cookie is a small file that asks permission to be placed on your computer's hard drive. Once you agree,
-            the
-            file
-            is added, and the cookie helps analyze web traffic or lets you know when you visit a particular site.
-            Cookies
-            allow
-            web applications to respond to you as an individual. The web application can tailor its operations to your
-            needs,
-            likes, and dislikes by gathering and remembering information about your preferences.
-        </p>
-
-        <p className="mb-4">
-            Overall, cookies help us provide you with a better website by enabling us to monitor which pages you find
-            useful
-            and which you do not. A cookie in no way gives us access to your computer or any information about you,
-            other
-            than
-            the data you choose to share with us.
-        </p>
-
+<li> W niektórych sytuacjach Administrator ma prawo przekazywać Twoje dane osobowe innym odbiorcom, jeśli będzie to niezbędne do wykonania zawartej z Tobą umowy lub do zrealizowania obowiązków ciążących na Administratorze. Dotyczy to takich grup odbiorców: </li>
+<li>  Firma hostingowa na zasadzie powierzenia. </li>
+<li>   Twoje dane osobowe przetwarzane przez Administratora nie dłużej, niż jest to konieczne do wykonania związanych z nimi czynności określonych osobnymi przepisami (np. o prowadzeniu rachunkowości). W odniesieniu do danych marketingowych dane nie będą przetwarzane dłużej niż przez 3 lata.  </li>
+<li> Przysługuje Ci prawo żądania od Administratora: dostępu do danych osobowych Ciebie dotyczących, ich sprostowania, usunięcia, ograniczenia przetwarzania oraz przenoszenia danych. </li>
+<li> Przysługuje Ci prawo do złożenia sprzeciwu w zakresie przetwarzania wskazanego w pkt 3.2 wobec przetwarzania danych osobowych w celu wykonania prawnie uzasadnionych interesów realizowanych przez Administratora, w tym profilowania, przy czym prawo sprzeciwu nie będzie mogło być wykonane w przypadku istnienia ważnych prawnie uzasadnionych podstaw do przetwarzania, nadrzędnych wobec Ciebie interesów, praw i wolności, w szczególności ustalenia, dochodzenia lub obrony roszczeń. </li>
+<li> Na działania Administratora przysługuje skarga do Prezesa Urzędu Ochrony Danych Osobowych, ul. Stawki 2, 00-193 Warszawa.  </li>
+<li>  Podanie danych osobowych jest dobrowolne, lecz niezbędne do obsługi Serwisu. </li>
+<li> Informacje o adresie IP. </li>
+<li> W stosunku do Ciebie mogą być podejmowane czynności polegające na zautomatyzowanym podejmowaniu decyzji, w tym profilowaniu w celu świadczenia usług w ramach zawartej umowy oraz w celu prowadzenia przez Administratora marketingu bezpośredniego. </li>
+<li>  Dane osobowe nie są przekazywane od krajów trzecich w rozumieniu przepisów o ochronie danych osobowych. Oznacza to, że nie przesyłamy ich poza teren Unii Europejskiej. </li>
+     </ul>
         <h2 className="text-2xl font-bold mb-2"> Informacje w formularzach</h2>
-
-        <p className="mb-4">
-            Our website may contain links to other websites of interest. However, once you have used these links to
-            leave
-            our
-            site, you should note that we do not have any control over that other website. Therefore, we cannot be
-            responsible
-            for the protection and privacy of any information which you provide whilst visiting such sites and such
-            sites
-            are
-            not governed by this privacy statement. You should exercise caution and look at the privacy statement
-            applicable
-            to
-            the website in question.
-        </p>
+          
+           <ul className="list-disc list-inside mb-4">
+<li>  Serwis zbiera informacje podane dobrowolnie przez użytkownika, w tym dane osobowe, o ile zostaną one podane.  </li>
+<li>  Serwis może zapisać informacje o parametrach połączenia (oznaczenie czasu, adres IP).  </li>
+<li>  Serwis, w niektórych wypadkach, może zapisać informację ułatwiającą powiązanie danych w formularzu z adresem e-mail użytkownika wypełniającego formularz. W takim wypadku adres e-mail użytkownika pojawia się wewnątrz adresu URL strony zawierającej formularz.  </li>
+<li> Dane podane w formularzu są przetwarzane w celu wynikającym z funkcji konkretnego formularza, np. w celu dokonania procesu obsługi zgłoszenia serwisowego lub kontaktu handlowego, rejestracji usług itp. Każdorazowo kontekst i opis formularza w czytelny sposób informuje, do czego on służy.  </li>
+</ul>
 
         <h2 className="text-2xl font-bold mb-2"> Logi Administratora</h2>
 
-        <p className="mb-4">
-            You may choose to restrict the collection or use of your personControlling Your Personal al information in the following ways:
-        </p>
+       
 
         <ul className="list-disc list-inside mb-4">
-            <li>If you have previously agreed to us using your personal information for direct marketing purposes, you
-                may
-                change your mind at any time by writing to or emailing us at [email protected]</li>
-            <li>We will not sell, distribute, or lease your personal information to third parties unless we have your
-                permission
-                or are required by law to do so. We may use your personal information to send you promotional
-                information
-                about
-                third parties which we think you may find interesting if you tell us that you wish this to happen.</li>
-            <li>You may request details of personal information which we hold about you. If you would like a copy of the
-                information held on you, please write to [Your Company Name, Address, City, State, Zip Code] or email
-                [email protected]</li>
-            <li>If you believe that any information we are holding on you is incorrect or incomplete, please write to or
-                email
-                us as soon as possible at the above address. We will promptly correct any information found to be
-                incorrect.
+            <li>
+                Informacje zachowaniu użytkowników w serwisie mogą podlegać logowaniu. Dane te są wykorzystywane w celu administrowania serwisem.
+
             </li>
         </ul>
         <h2 className="text-2xl font-bold mb-2">  Istotne techniki marketingowe</h2>
+        <ul className="list-disc list-inside mb-4">
+
+<li>  Operator stosuje analizę statystyczną ruchu na stronie, poprzez Google Analytics (Google Inc. z siedzibą w USA). Operator nie przekazuje do operatora tej usługi danych osobowych, a jedynie zanonimizowane informacje. Usługa bazuje na wykorzystaniu ciasteczek w urządzeniu końcowym użytkownika. W zakresie informacji o preferencjach użytkownika gromadzonych przez sieć reklamową Google użytkownik może przeglądać i edytować informacje wynikające z plików cookies przy pomocy narzędzia:
+https://www.google.com/ads/preferences/  
+  </li>
+<li>  Operator stosuje techniki remarketingowe, pozwalające na dopasowanie przekazów reklamowych do zachowania użytkownika na stronie, co może dawać złudzenie, że dane osobowe użytkownika są wykorzystywane do jego śledzenia, jednak w praktyce nie dochodzi do przekazania żadnych danych osobowych od Operatora do operatorom reklam. Technologicznym warunkiem takich działań jest włączona obsługa plików cookie.  </li>
+<li>  Operator stosuje korzysta z piksela Facebooka. Ta technologia powoduje, że serwis Facebook (Facebook Inc. z siedzibą w USA) wie, że dana osoba w nim zarejestrowana korzysta z Serwisu. Bazuje w tym wypadku na danych, wobec których sam jest administratorem, Operator nie przekazuje od siebie żadnych dodatkowych danych osobowych serwisowi Facebook. Usługa bazuje na wykorzystaniu ciasteczek w urządzeniu końcowym użytkownika.   </li>
+<li> Operator stosuje rozwiązanie badające zachowanie użytkowników poprzez tworzenie map ciepła oraz nagrywanie zachowania na stronie. Te informacje są anonimizowane zanim zostaną przesłane do operatora usługi tak, że nie wie on jakiej osoby fizycznej one dotyczą. W szczególności nagrywaniu nie podlegają wpisywane hasła oraz inne dane osobowe.   </li>
+<li>  Operator stosuje rozwiązanie automatyzujące działanie Serwisu w odniesieniu do użytkowników, np. mogące przesłać maila do użytkownika po odwiedzeniu konkretnej podstrony, o ile wyraził on zgodę na otrzymywanie korespondencji handlowej od Operatora.   </li>
+<li>   Operator może stosować profilowanie w rozumieniu przepisów o ochronie danych osobowych.   </li>
+            
+        </ul>
 
                 <h2 className="text-2xl font-bold mb-2">   Informacja o plikach cookies</h2>
+        <ul className="list-disc list-inside mb-4">
+
+<li>  Serwis korzysta z plików cookies.  </li>
+<li> Pliki cookies (tzw. „ciasteczka”) stanowią dane informatyczne, w szczególności pliki tekstowe, które przechowywane są w urządzeniu końcowym Użytkownika Serwisu i przeznaczone są do korzystania ze stron internetowych Serwisu. Cookies zazwyczaj zawierają nazwę strony internetowej, z której pochodzą, czas przechowywania ich na urządzeniu końcowym oraz unikalny numer.  </li>
+<li>  Podmiotem zamieszczającym na urządzeniu końcowym Użytkownika Serwisu pliki cookies oraz uzyskującym do nich dostęp jest operator Serwisu.  </li>
+<li>  Pliki cookies wykorzystywane są w następujących celach: 
+     Utrzymanie sesji użytkownika Serwisu (po zalogowaniu), dzięki której użytkownik nie musi na każdej podstronie Serwisu ponownie wpisywać loginu i hasła.  Realizacja celów określonych powyżej w części „Istotne techniki marketingowe”.   </li>
+<li> W ramach Serwisu stosowane są dwa zasadnicze rodzaje plików cookies: „sesyjne” (session cookies) oraz „stałe” (persistent cookies). Cookies „sesyjne” są plikami tymczasowymi, które przechowywane są w urządzeniu końcowym Użytkownika do czasu wylogowania, opuszczenia strony internetowej lub wyłączenia oprogramowania (przeglądarki internetowej). „Stałe” pliki cookies przechowywane są w urządzeniu końcowym Użytkownika przez czas określony w parametrach plików cookies lub do czasu ich usunięcia przez Użytkownika.  </li>
+<li>  Oprogramowanie do przeglądania stron internetowych (przeglądarka internetowa) zazwyczaj domyślnie dopuszcza przechowywanie plików cookies w urządzeniu końcowym Użytkownika. Użytkownicy Serwisu mogą dokonać zmiany ustawień w tym zakresie. Przeglądarka internetowa umożliwia usunięcie plików cookies. Możliwe jest także automatyczne blokowanie plików cookies Szczegółowe informacje na ten temat zawiera pomoc lub dokumentacja przeglądarki internetowej. </li>
+<li>   Ograniczenia stosowania plików cookies mogą wpłynąć na niektóre funkcjonalności dostępne na stronach internetowych Serwisu.   </li>
+<li>   Pliki cookies zamieszczane w urządzeniu końcowym Użytkownika Serwisu wykorzystywane mogą być również przez współpracujące z operatorem Serwisu podmioty, w szczególności dotyczy to firm: Google (Google Inc. z siedzibą w USA), Facebook (Facebook Inc. z siedzibą w USA), Twitter (Twitter Inc. z siedzibą w USA).   </li>
+
+  </ul>
+            
+               <h2 className="text-2xl font-bold mb-2">   Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę</h2>
+      
+        <ul className="list-disc list-inside mb-4">
+<li>   Jeśli użytkownik nie chce otrzymywać plików cookies, może zmienić ustawienia przeglądarki. Zastrzegamy, że wyłączenie obsługi plików cookies niezbędnych dla procesów uwierzytelniania, bezpieczeństwa, utrzymania preferencji użytkownika może utrudnić, a w skrajnych przypadkach może uniemożliwić korzystanie ze stron www   </li>
+<li>   W celu zarządzania ustawienia cookies wybierz z listy poniżej przeglądarkę internetową, której używasz i postępuj zgodnie z instrukcjami:  </li>
+<li>  Serwis korzysta z plików cookies.  </li>
+<li>  Serwis korzysta z plików cookies.  </li>
+<li>  Serwis korzysta z plików cookies.  </li>
+<li>  Serwis korzysta z plików cookies.  </li>
+<li>  Serwis korzysta z plików cookies.  </li>
 
 
-                <h2 className="text-2xl font-bold mb-2">   Zarządzanie plikami cookies – jak w praktyce wyrażać i cofać zgodę</h2>
-
-
+  </ul>
         <p className="mb-4">
             This privacy policy is subject to change without notice.
         </p>
