983bfcef7425bddbe7bac6d2f6259166b647f3c8
MaccSob <71sobmaciej@gmail.com>
2026-01-24 23:57:19 +0100

    r

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -1,57 +1,123 @@
-import { HiMiniBookOpen } from "react-icons/hi2";
-import Link from 'next/link';
-import { ThemeToggle } from '../theme-toggle';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { ThemeToggle } from "../theme-toggle";
 
 export default function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Navigation items array
+  const navItems = [
+    { name: "O mnie", href: "/about" },
+    { name: "Portfolio", href: "/portfolio" },
+    { name: "Contact", href: "/contact" },
+    
+  ];
+
   return (
-  
-    <>
-    <nav className='fixed z-1 top-0 w-full flex items-center justify-between py-5 px-24 border-b border-gray-700 bg-white dark:bg-black text-black dark:text-white transition-all duration-750 ease-out bg-[url(/public/mountains.jpg)]'>
+    <div>
+      <nav className="block w-full max-w-screen px-4 py-4 mx-auto bg-white bg-opacity-90 sticky top-3 shadow lg:px-8 backdrop-blur-lg backdrop-saturate-150 z-[9999] dark:bg-black text-white dark:text-white transition-all duration-750 ease-out">
+        <div className="container flex flex-wrap items-center justify-between mx-auto text-slate-800">
+          <Link
+            href="/"
+            className="mr-4 block cursor-pointer py-1.5 text-black-600 font-bold text-2xl"
+          >
+            Strona po Stronie
+          </Link>
 
+          <div className="lg:hidden">
+            <button
+              className="relative ml-auto h-6 max-h-[40px] w-6 max-w-[40px] select-none rounded-lg text-center align-middle text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              onClick={toggleMobileMenu}
+              type="button"
+            >
+              <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </span>
+            </button>
+          </div>
 
-    <Link href='/' className='text-black-300 hover:text-green transition-colors text-2xl font-medium  flex items-center gap-3 relative cursor-pointer hover:translate-y-0.75'>
-    <HiMiniBookOpen size={35}/>
-    Strona po stronie 
-    </Link>
+          {/* Mobile Menu */}
+          <div
+            className={`fixed top-0 left-0 min-h-screen w-64 bg-slate-100 shadow-lg transform transition-transform duration-300 ease-in-out dark:bg-black text-black dark:text-white transition-all duration-750 ease-out ${
+              isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
+            } lg:hidden z-50`}
+          >
+            <div className="flex flex-row items-center border-b pb-4">
+              <Link
+                href="/"
+                className="cursor-pointer text-green-800 font-bold text-xl pt-4 ps-4"
+              >
+                Strona po Stronie
+              </Link>
+              <button
+                onClick={toggleMobileMenu}
+                className="absolute top-4 right-4 text-slate-600 hover:text-red-500"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-8 h-8"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <ul className="flex flex-col h-full gap-4 p-4">
+              {navItems.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex items-center p-1 text-lg gap-x-2 text-slate-600 hover:text-green-800"
+                >
+                  <Link onClick={() => {setIsMobileMenuOpen(false);}} href={item.href} className="flex items-center">
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-    <ul className='flex gap-10 text-lg'>
-
-
-    <Link href='/about' className='text-black-300 hover:text-green transition-colors text-1xm font-medium relative after:bg-green-400 after:absolute after:h-1 after:w-0 after:bottom-2 after:left-0 hover:after:w-full after:transition-all after:duration-750 cursor-pointer hover:translate-y-0.75'>
-        O mnie
-    </Link>
-
-
-
-
-
-     <Link href='/portfolio' className='text-black-300 hover:text-green transition-colors text-1xm font-medium relative after:bg-green-400 after:absolute after:h-1 after:w-0 after:bottom-2 after:left-0 hover:after:w-full after:transition-all after:duration-750 cursor-pointer hover:translate-y-0.75' >
-        Portfolio
-    </Link>
-
-
-     <Link href='/contact' className='text-black-300 hover:text-green transition-colors text-1xm font-medium relative after:bg-green-400 after:absolute after:h-1 after:w-0 after:bottom-2 after:left-0 hover:after:w-full after:transition-all after:duration-750 cursor-pointer hover:translate-y-0.75'>
-        Kontakt
-    </Link>
-
-
-
-
-        <Link href='/products' className='text-black-300 hover:text-green transition-colors text-1xm font-medium relative after:bg-green-400 after:absolute after:h-1 after:w-0 after:bottom-2 after:left-0 hover:after:w-full after:transition-all after:duration-750 cursor-pointer hover:translate-y-0.75'>
-        Oferta
-    </Link>
- 
-     <div className="space-x-2">
-     </div>
-     <ThemeToggle></ThemeToggle>
-    </ul>
-
-
-    </nav>
-
-
-
-    </>
-
+          {/* Desktop Menu */}
+          <div className="hidden lg:block">
+            <ul className="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+              
+              {navItems.map((item, index) => (
+                <li
+                  key={index}
+                  className="flex items-center p-1 text-lg gap-x-2 text-slate-600 hover:text-green-800"
+                >
+                  <Link href={item.href} className="flex items-center">
+                    {item.name}
+                  </Link>
+                </li>
+                
+              ))}
+                                                <ThemeToggle/>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
