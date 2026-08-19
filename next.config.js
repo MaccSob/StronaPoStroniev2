a0456d69612d4064b3c93e64e03d659d5b494783
MaccSob <71sobmaciej@gmail.com>
2026-08-19 18:39:26 +0200

    444

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-
+// Ta sama wartosc jest ustawiana w workflow GitHub Actions (env: NEXT_PUBLIC_BASE_PATH)
+// i uzywana zarowno tutaj (dla routingu/CSS/JS), jak i w komponentach (dla obrazkow z next/image).
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig = {
